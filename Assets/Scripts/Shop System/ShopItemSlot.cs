58e4a01eaa31fcdf04724555eb6dfaa5d1524892
ELDamien3570 <damienhelm@gmail.com>
2025-08-08 04:05:45 -0400

    ItemHoverTooltip

--- Assets/Scripts/Shop System/ShopItemSlot.cs
+++ Assets/Scripts/Shop System/ShopItemSlot.cs
@@ -10,7 +10,7 @@ public class ShopItemSlot : MonoBehaviour
     [SerializeField] private TextMeshProUGUI priceText;
     [SerializeField] private Button buyButton;
 
-    private InventoryItem itemData;
+    public InventoryItem itemData;
     private float itemPrice;
 
     public void Initialize(InventoryItem item)
